15e67d58af9aa6a6a613afa6cf20c625dda25462
nilesh252000 <nileshraut2507@gmail.com>
2024-10-07 15:35:06 +0530

    Pattern11 file added

--- java/PatternProgrameuseNestedForloop/Pattern11.java
+++ java/PatternProgrameuseNestedForloop/Pattern11.java
@@ -2,56 +2,37 @@ package PatternProgrameuseNestedForloop;
 
 public class Pattern11 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         for(int  i=1;i<=4;i++)
-                {
-                    for(int j=1;j<=4;j++)
-                    {
-                        // System.out.print("*");
-                
-                        if(j<=i)
-                        {
-                            System.out.print(j);
-                        }
-                        else
-                        {
-                            System.out.print(" ");
-                        }
-                    }
-                    int k=0;
-                        for(int j=4;j>=1;j--)
-                    {
-                        // System.out.print("*");
-                
-                        if(j<=i)
-                        {
-                            System.out.print(++k);
-                        }
-                        else
-                        {
-                            System.out.print(" ");
-                        }
-                    }
-                    System.out.println();
-                }
-                // for(int  i=5;i>=1;i--)
-                // {
-                //     int k=0;
-                //     for(int j=1;j<=5;j++)
-                //     {
-                //         // System.out.print("*");
-                
-                //         if(j>=i)
-                //         {
-                //             System.out.print(++k);
-                //         }
-                //         else
-                //         {
-                //             System.out.print(" ");
-                //         }
-                //     }
-                //     System.out.println();
-                // }
+        {
+            for(int j=1;j<=4;j++)
+        {
+            // System.out.print("*");
+            if(j<=i)
+            {
+                System.out.print(j);
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+        int k=0;
+        for(int j=4;j>=1;j--)
+        {
+            // System.out.print("*");
+            if(j<=i)
+            {
+                System.out.print(++k);
+            }
+            else
+            {
+                    System.out.print(" ");
+            }
+        }
+        System.out.println();
     }
-    
 }
+}
+    
