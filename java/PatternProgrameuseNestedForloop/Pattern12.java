36478281556ecd40228c749e9f85a09a86c1a6a7
nilesh252000 <nileshraut2507@gmail.com>
2024-10-07 15:08:04 +0530

    Pattern12 file added

--- java/PatternProgrameuseNestedForloop/Pattern12.java
+++ java/PatternProgrameuseNestedForloop/Pattern12.java
@@ -2,5 +2,28 @@ package PatternProgrameuseNestedForloop;
 
 public class Pattern12 
 {
+    public static void main(String[] args) 
+    {
+        for(int i=4;i>=1;i--)
+        {
+            int k=1;
+            for(int j=1;j<=4;j++)
+            {
+                if(j>=i)
+                {
+                    System.out.print(k++);
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println("");
+        }
+        
+    }
     
-}
+    }
+
+
